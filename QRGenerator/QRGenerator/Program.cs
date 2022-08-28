a2a676a538bcce8589813ad03a0d8ae03a8a6b34
MARDOM\mcrodriguez <marckorv87@gmail.com>
2022-08-28 15:23:35 -0400

    first commit

---
+++ QRGenerator/QRGenerator/Program.cs
@@ -0,0 +1,14 @@
+﻿using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+
+string UrlQR = "https://linktr.ee/wakari";
+string imagePath = "img.png";
+
+
+    QRCodeGenerator qRCodeGenerator = new QRCodeGenerator();
+    QRCodeData qRCodeData = qRCodeGenerator.CreateQrCode(UrlQR, QRCodeGenerator.ECCLevel.Q);
+    QRCode qRCode = new QRCode(qRCodeData);
+    Bitmap qrCodeImage = qRCode.GetGraphic(10, Color.DarkCyan, Color.White, (Bitmap)Bitmap.FromFile(imagePath),25,20);
+    qrCodeImage.Save(@"QRImage.bmp");
